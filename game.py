2919a53c1f2d18655031eb960a133240a59e0f32
Egorcherno <79221644+Egorcherno@users.noreply.github.com>
2021-05-08 15:30:24 +0300

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,15 @@ W = 1366
 H = 768
 win = display.set_mode((W,H),FULLSCREEN)
 
+class Main(sprite.Sprite):
+    def __init__(self,x,y,pic,W,H,speed):
+        super().__init__()
+        self.pic = image.load(pic)
+        self.rect = Rect(x,y,W,H)
+    def reset(self):
+        win.blit(self.pic,(self.rect.x,self.rect.y))
 
+ball = Main(300,300,'ball.png',40,40,50)
 
 def control():
     global game
@@ -13,11 +21,15 @@ def control():
         if e.type == 2:
             if e.key == K_ESCAPE:
                 game = False
+        if e.type == 12:
+            game = False
+
+
 
 while game:
     control()
     win.fill((255,255,255))
-
+    ball.reset()
 
 
 
